2dfad84b06aea50dfd576933ad9ef0582f167b72
Anime-boi <max201-3>
2019-07-08 17:41:58 +0300

    Functions called for char

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void FillRand(int Arr[], const int n);
-void Print(int Arr[], const int n);
+template<typename T>void FillRand(T Arr[], const int n);
+
+template<typename T>void Print(T Arr[], const int n);
+
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	const int n = 5;
+	int Arr[n];
+	FillRand(Arr, n);
+	Print(Arr, n);
 
-
+	char Brr[n];
+	FillRand(Arr, n);
+	Print(Arr, n);
 }
 
-void FillRand(int Arr[], const int n)
+template<typename T>void FillRand(T Arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		cout << Arr[i] << "\t";
+		Arr[i] = rand() % 100;
 	}
-	cout << endl; 
 }
-void Print(int Arr[], const int n)
+template <typename T>void Print(T Arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
